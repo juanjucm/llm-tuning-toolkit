5a91e45df0aa9bbd139f8391686ff16fcf364aaf
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2026-02-10 13:37:48 +0100

    Fix cache dir argument to use `Path.home()` instead of `~`

--- src/auto_tune/cli.py
+++ src/auto_tune/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+from pathlib import Path
 
 from auto_tune.tuner import AutoTuner
 
@@ -10,8 +11,14 @@ parser = argparse.ArgumentParser(description="Auto-tune tool for finding optimal
 parser.add_argument("--config", help="Path to auto-tune configuration file", required=True)
 parser.add_argument("--result-dir", default="", help="Directory to save tuning results")
 parser.add_argument("--dataset-id", help="Huggingface dataset where to dump resutls")
-parser.add_argument("--cache-dir", default="~/.cache/huggingface/hub", help="Cache directory for Huggingface models and datasets.")
-parser.add_argument("--hf-token", default=HF_TOKEN, help="Huggingface token to use for accesing models and dataset.")
+parser.add_argument(
+    "--cache-dir",
+    default=str(Path.home() / ".cache" / "huggingface" / "hub"),
+    help="Cache directory for Huggingface models and datasets.",
+)
+parser.add_argument(
+    "--hf-token", default=HF_TOKEN, help="Huggingface token to use for accesing models and dataset."
+)
 
 
 def main() -> None:
